20fe5d6e214a3336583e5aaf74cf92596b40ff46
NolongerHumanOrKilua <108425756+NolongerHumanOrKilua@users.noreply.github.com>
2022-10-21 10:59:34 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+# Электронный дневник школы
+
+Программа для помощи Ване по внесению изменений в дневник школы
+
+## Функция fix_marks(schoolkid)
+
+Функция fix_marks(schoolkid) изменяет оценки с 2 и 3 на 5
+Пример использования:
+```python
+fix_marks("Фролов Иван")
+```
+
+## Функция remove_chastisements(schoolkid)
+
+Функция remove_chastisements(schoolkid) убирает из школьного дневника замечания ученика
+```python
+remove_chastisements("Фролов Иван")
+```
+
+## Функция create_commendation(schoolkid, 'Музыка')
+```python
+make_commendation("Фролов Иван", "Музыка")
+```
+
+### Работа программы
+
+Чтобы запустить скрипт можно его целиком скопировать и вставить в shell
---
+++ scripts.py
@@ -0,0 +1,25 @@
+import random
+
+commendations=["Ты меня приятно удивил!", "Сказано здорово – просто и ясно!", "Ты многое сделал, я это вижу!", "Теперь у тебя точно все получится!", "Здорово!", "Талантливо!"]
+subject_name="Музыка"
+schoolkid="Фролов Иван"
+def fix_marks(schoolkid):
+  child=Schoolkid.objects.get(full_name__contains=schoolkid)
+  marks=Mark.objects.filter(schoolkid = child, points__in=[2,3])
+  for mark in marks:
+    mark.points=5
+    mark.save()
+	
+remove_chastisements(schoolkid):
+   child=Schoolkid.objects.get(full_name__contains=schoolkid)
+   chastisement=Chastisement.objects.filter(schoolkid=child)
+   chastisement.delete()
+   
+create_commendation(schoolkid, subject_name)
+	child=Schoolkid.objects.get(full_name__contains=schoolkid)
+	subject=Subject.objects.get(title__contains=subject_name, year_of_study=child.year_of_study)
+	lesson=Lesson.objects.filter(group_letter=child.group_letter, subject=subject).order_by('?')[0]
+	teacher=lesson.teacher
+	subject=lesson.subject
+	date=lesson.date
+	Commendation.objects.create(text=random.choise(commendations), created=date, schoolkid=child, subject=subject, teacher=teacher)
